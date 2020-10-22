4497cff423fbfeaf2653fe552a8eb2c28ec02fc7
Vladislav Avramenko <39153301+AvraamV@users.noreply.github.com>
2020-10-22 13:53:15 +0300

    final main

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,26 @@ jQuery(document).ready(function ($) {
 		infinite: false,
 		dots: true
 	});
+
+	$('.benefits_slider').slick({
+		arrows: false,
+		dots: true
+	});
+
+	$('.package_slider').slick({
+		slidesToShow: 2,
+		arrows: false,
+		dots: true,
+		infinite: false,
+		responsive: [
+			{
+				breakpoint: 575,
+				settings:{
+					slidesToShow: 1
+				}
+			}
+		]
+	});
 	
 	$('.current_lang').on('click', function(){
 		$(this).siblings('.another_lang').fadeToggle(200);
@@ -18,5 +38,29 @@ jQuery(document).ready(function ($) {
 	  });
 	});
 
+	$('.benefit_item').on('click', function(){
+		$(this).toggleClass('clicked');
+		$(this).find('.frontside').toggleClass('clicked');
+		$(this).find('.backside').toggleClass('clicked');
+	});
 
+
+	$('.question_btn').on('click', function(){
+		$(this).toggleClass('open');
+		$(this).siblings('p').slideToggle();
+	});
+
+	$('.footer_item h3').on('click', function(){
+		if($(window).width() < 576){
+			$(this).toggleClass('show');
+			$(this).siblings('ul').slideToggle();
+		}
+	});
+
+	$('.menu_btn').on('click', function(e){
+		e.preventDefault();
+		$(this).toggleClass('open');
+		$('header .footer_list').toggleClass('open');
+	})
+	
 });
